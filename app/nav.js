60915f7f7b6790ff42a6aacedc97010d5e232cb9
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-08 14:46:15 -0300

    agregar modulo de navegacion

---
+++ app/nav.js
@@ -0,0 +1,11 @@
+const Nav = () =>
+`<nav class="navbar navbar-dark">
+    <ul class="navbar-nav">
+        <li><a class="nav-link" href="/">Inicio</a></li>
+        <li><a class="nav-link" href="/about">Acerca</a></li>
+        <li><a class="nav-link" href="/gallery">Galeria</a></li>
+        <li><a class="nav-link" href="/contact">Contacto</a></li>
+    </ul>
+</nav>`
+
+module.exports = Nav;
--- index.js
+++ index.js
@@ -1,24 +1,38 @@
+// Modulos Core NODEJS
 const http = require('node:http');
-
+// Datos de Conexion
 const HOST = process.env.HOST ?? "127.0.0.1";
 const PORT = process.env.PORT ?? 3000;
-
+// Modulos de Aplicacion
+const nav = require('./app/nav');
+// Servidor LOCAL NodeJS
 const server = http.createServer((req, res) => {
     const { url, method } = req;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+    res.write(nav());
     switch(url.toLowerCase()){
     case '/':
-        res.write(`<h1>Pagina Principal</h1>`);
-        break;
+    case '/index':
+    case '/index.html':
+        res.end(`<h1>Página Principal</h1>`);
+    break;
     case "/contact":
-        res.write(`<h1>Pagina de Contacto</h1>`);
-        break;
+    case "/contact.html":
+        res.end(`<h1>Contactanos</h1>`);
+    break;
     case "/about":
-        res.write(`<h1>Pagina Acerca de</h1>`);
-        break;
+        res.end(`<h1>Sobre Nosotros</h1>`);
+    break;
+    case "/gallery":
+        res.end("<h1>Galería de Imágenes</h1>");
+    break;
     default:
-        res.write(`<h1>Error 404<br> Pagina ${url} no encontrada</h1>`)
+        res.end(`<h1>Error 404<br> Página ${url} no encontrada</h1>`)
     }
 });
 
-server.listen(PORT, HOST, () => console.log(`la aplicacion esta funcionando en la direccion ${HOST}:${PORT}`))
+server.listen(
+    PORT, // Puerto utilizado
+    HOST, // Direccion IP o nombre de HOST
+    () => console.log(`la aplicacion esta funcionando en la direccion ${HOST}:${PORT}`) // callback
+)
