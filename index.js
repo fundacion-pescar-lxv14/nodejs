6c0aca7c659b92af4298efb834f115928d185347
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-10 14:20:19 -0300

    manipular metodos http

--- index.js
+++ index.js
@@ -7,60 +7,71 @@ const PORT = process.env.PORT ?? 3000;
 // Modulos de Aplicacion
 const Content = require('./app/content');
 // Servidor LOCAL NodeJS
-const server = http.createServer((req, res) => {
+let counter = 0;
+const server = http.createServer((req, res) => {        
     let 
         file = "index.html",
         notFound = false,
-        counter = 0,
         title = "";
-
     const
         dir = './public',
         head = { 
             'Content-Type': "text/html; charset=utf-8",
             'desarrollador': "Cristian Racedo & compañia"
         }
-    switch(req.url.toLowerCase()){
-        case '/':
-        case '/index':
-        case '/index.html':
-            title="<h1>Te damos la Bienvenida</h1>";
-        break;
-        case "/contact":
-        case "/contact.html":
-            title ="<h1>Contactanos</h1>";
-        break;
-        case "/about":
-            title ="<h1>Sobre Nosotros</h1>";
-        break;
-        case "/gallery":
-            title ="<h1>Galería de Imágenes</h1>";
-        break;
-        case "/styles":
-        case "/styles.css":
-        case "/styles/default.css":
-        case "/css/styles.css":
-            file = "styles/default.css";
-            head['Content-Type'] = "text/css; charset=utf-8";
-        break;
-        default:
-            title =`<h1>Error 404<br> Página ${req.url} no encontrada</h1>`
-            notFound = true;
-    }
-    if (notFound) {
-        res.writeHead(404, head);
-        res.end(title);
-    }
-    else readFile(`${dir}/${file}`, (err, data) => {
-        if (err){
-            res.writeHead(500, head);
-            res.end(`<h1>Error 500<br> Internal Server Error</h1>`);
+    counter++
+    if(req.method == "GET"){
+
+        switch(req.url.toLowerCase()){
+            case '/':
+            case '/index':
+            case '/index.html':
+                title="<h1>Te damos la Bienvenida</h1>";
+            break;
+            case "/contact":
+            case "/contact.html":
+                title ="<h1>Contactanos</h1>";
+            break;
+            case "/about":
+                title ="<h1>Sobre Nosotros</h1>";
+            break;
+            case "/gallery":
+                title ="<h1>Galería de Imágenes</h1>";
+            break;
+            case "/styles":
+            case "/styles.css":
+            case "/styles/default.css":
+            case "/css/styles.css":
+                file = "styles/default.css";
+                head['Content-Type'] = "text/css; charset=utf-8";
+            break;
+            default:
+                title =`<h1>Error 404<br> Página ${req.url} no encontrada</h1>`
+                notFound = true;
         }
-        else {
-            res.writeHead(200, head);
-            res.end(Content(data,{ title, ...req })) 
+        if (notFound) {
+            res.writeHead(404, head);
+            res.end(title);
         }
-    })
+        else readFile(`${dir}/${file}`, (err, data) => {
+            if (err){
+                res.writeHead(500, head);
+                res.end(`<h1>Error 500<br> Internal Server Error</h1>`);
+            }
+            else {
+                res.writeHead(200, head);
+                res.end(Content(data,{ counter, title, ...req })) 
+            }
+        })
+    }
+    else if (req.method == "POST"){
+        res.writeHead(200, head)
+        res.end(`<p>La solicitud fue procesada con exito</p>`)
+    }
+    else{
+        res.writeHead(405, head)
+        res.end(`<p>Metodo ${req.method} no permitido</p>`)
+    }
 });
 
 server.listen(
