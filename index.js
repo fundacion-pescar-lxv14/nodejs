c430c155ee40d6ee0e5eb4d09f2532ba8d81c5b5
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-13 19:49:38 -0300

    implementar arquitectura mrc de usuarios

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import express from 'express'
+import userRouter from './routes/users.js'
+import { urlencoded } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
+app.use(urlencoded({extended: true}));
 
 // Rutas Estaticas para el Frontend
 app.use("/", express.static("public"));
-
 // Rutas de la API
-/**
- * posts/ -> Muestra todos los productos
- * posts/:id -> Muestra el producto con el id elegido
- * posts/category/:category -> muestra los productos de la categoria elegida
- * posts/category/:category/:id -> muestra el producto de la categoria elegida con el id elegido
- */
+app.use("/users", userRouter );
 
 app.listen(PORT, console.log(startMsg))
