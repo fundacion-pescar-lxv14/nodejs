c7c0366a74ed836be43a776286931fdfcc636895
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-15 15:52:04 -0300

    implementacion de body parser

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import userRouter from './routes/users.js'
+import postRouter from './routes/posts.js'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
 
+// Request Body Parser (Middleware)
+app.use(express.urlencoded({extended: true}));
+
 // Rutas de la API
 app.use("/users", userRouter );
+app.use("/posts", postRouter );
 
 // Rutas Estaticas para el Frontend
 app.use("/", express.static("public"));
