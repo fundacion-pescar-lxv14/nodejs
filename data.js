e4813919b8c7e0aed06d2eccd65819231445b0a9
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-17 15:53:04 -0300

    actualizar maqueta de datos de aplicacion

--- data.js
+++ data.js
@@ -3,10 +3,31 @@ export const data = {
     descripcion: "Tienda de articulos de todo tipo",
     version: "0.1.0",
     autor: "Pescar SA",
-    menu: ["Inicio", "Productos", "Contacto"],
+    menu: ["home", "posts", "profile"],
     social: [
         { link: "https://facebook.com", icon: "fa-facebook" },
         { link: "https://twitter.com", icon: "fa-twitter" },
         { link: "https://instagram.com", icon: "fa-instagram" },
     ] 
+}
+export const user = {
+    name: "Cristian",
+    email: "Cristiandracedo@hotmail.com"
+}
+
+export const posts = {  
+    title: 'Posts',
+    description: 'All Posts',
+    posts: [
+        {
+            title: "articulo 1",
+            img: "ruta a la imagen",
+            description: ["el1", "el2", "el3"],
+            comments: [
+            {author: "pepe", body: 'muy bueno'},
+            {author: "pepe", body: 'muy bueno'},
+            {author: "pepe", body: 'muy bueno'}
+            ],
+        }
+    ]
 }
