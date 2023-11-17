3f2a5cb35746f58633154721ece2b5d8a7ad0696
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-17 14:31:59 -0300

    agregar archivos base para plantilla

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
 
 const data = {
     nombre: "Pescar Shop",
-    descripcion: "Tienda de articulos de pesca",
+    descripcion: "Tienda de articulos de todo tipo",
     version: "0.1.0",
     autor: "Pescar SA"
 }
+// Motor de Plantillas
+app.set("view engine", "pug")
+app.use("/", express.static("views"))
 
-app.get('/', (req, res) => { console.log(req, res) })
+app.get('/', (req, res) => { 
+  res.render('index', data)  
+})
 
 app.listen(PORT, console.log(startMsg))
