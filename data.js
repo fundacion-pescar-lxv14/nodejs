a51345a825f495ea5f8bb64c296a46b951e51c90
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-17 15:32:05 -0300

    generar archivo de datos

---
+++ data.js
@@ -0,0 +1,12 @@
+export const data = {
+    nombre: "Pescar Shop",
+    descripcion: "Tienda de articulos de todo tipo",
+    version: "0.1.0",
+    autor: "Pescar SA",
+    menu: ["Inicio", "Productos", "Contacto"],
+    social: [
+        { link: "https://facebook.com", icon: "fa-facebook" },
+        { link: "https://twitter.com", icon: "fa-twitter" },
+        { link: "https://instagram.com", icon: "fa-instagram" },
+    ] 
+}
