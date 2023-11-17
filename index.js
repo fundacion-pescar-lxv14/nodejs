0dbd95ac846eede124318cd3a1c3b7af592096b1
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-17 15:31:31 -0300

    implementar bucles y condicionales

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 import express from 'express';
+import { data } from './data.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
 
-const data = {
-    nombre: "Pescar Shop",
-    descripcion: "Tienda de articulos de todo tipo",
-    version: "0.1.0",
-    autor: "Pescar SA"
-}
 // Motor de Plantillas
 app.set("view engine", "pug")
 app.use("/", express.static("views"))
 
 app.get('/', (req, res) => { 
-  res.render('index', data)  
+  const content = {
+    title: 'Home',
+    description: 'Home page'
+  }
+  res.render('index', {...data, ...content})  
 })
 
 app.listen(PORT, console.log(startMsg))
