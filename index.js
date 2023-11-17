a00622d2a7c6b87c6a0eb2438066182ceaf0d7f9
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-17 15:53:04 -0300

    establecer estructura de rutas con ejs

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from 'express';
+import { data, posts, user } from './data.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
 
-const data = {
-    nombre: "Pescar Shop",
-    descripcion: "Tienda de articulos de pesca",
-    version: "0.1.0",
-    autor: "Pescar SA"
-}
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => { console.log(req, res) })
-
+// Perfile de Usuario
+app.get('/profile', (req, res) => res.render('index', {...data, user})) // Agregamos la clave user con sus valores
+// Publicaciones
+app.get('/posts', (req, res) => res.render('index', {...data, ...posts})) // Unificamos las claves de posts
+// Ruta General
+app.get('*', (req, res) => res.render("index", data))
 app.listen(PORT, console.log(startMsg))
