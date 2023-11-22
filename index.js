16b0acfe8c8a57264d1136bfb67bd05001402c78
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-22 15:33:43 -0300

    instalar e implementar exphbs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import { data, posts, user } from './data.js';
+import {engine} from 'express-handlebars';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
 
+const config = {
+    partialDir: 'views/partials',
+    layoutsDir: 'views/',
+    defaultLayout: 'index',
+    extname: 'hbs'
+}
 
+app.engine('handlebars', engine(config));
+app.set('view engine', 'handlebars');
 // Perfile de Usuario
 app.get('/profile', (req, res) => res.render('index', {...data, user})) // Agregamos la clave user con sus valores
 // Publicaciones
