4bc4e50ad9335043195f8c09ee1e67a10ccc8f4f
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-24 16:47:59 -0300

    instalar morgan y dotenv para entorno de desarrollo

--- config/db.js
+++ config/db.js
@@ -1,10 +1,22 @@
 // Importacion del ORM
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+dotenv.config()
 // Indicador de Recurso Universal (Servidor MongoDB)
-const APP_URI = process.env.APP_URI ?? 'mongodb://localhost:27017/pescar_shop'
+const { 
+    DBTYPE, 
+    USERNAME, 
+    PASSWORD, 
+    HOSTNAME, 
+    DB_PORT, 
+    DATABASE, 
+    APP_URI,
+    WEB_URI
+} = process.env;
+const URI = APP_URI ?? `${DBTYPE}://${HOSTNAME}:${DB_PORT}/${DATABASE}`;
 // Conexion con Servidor de Base de Datos
-const dbConn = mongoose.connect(APP_URI)
-.then(() => console.log('conectado con la base de datos'))
+const dbConn = mongoose.connect(URI)
+.then(() => console.log('conectado con la base de datos ' + DATABASE))
 .catch(err => console.log(err));
 // Exportacion de la Conexion
 export default dbConn;
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import dbConn from './config/db.js';
 import userRouter from './routes/users.js'
 import postRouter from './routes/posts.js'
+import morgan from 'morgan';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
 
+app.use(morgan('dev'));
 // Request Body Parser (Middleware)
 app.use(express.urlencoded({extended: true}));
 // Rutas de la API
