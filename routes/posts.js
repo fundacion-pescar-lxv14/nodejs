d91de8ba8a1adea3aec129f9966a15439aa35c85
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-24 15:35:30 -0300

    actualizar rutas de publicacion

--- routes/posts.js
+++ routes/posts.js
@@ -4,12 +4,12 @@ import * as P from '../controllers/posts.js'
 const postRouter = Router();
 
 // Ruta Alternativas
-postRouter.get      ( '/:field/:id', P.getPosts ); // Props tipo String
+postRouter.get      ( '/:field/:postId', P.getFilteredPosts ); // Props tipo String
 postRouter.get      ( '/filter', P.getFilteredPosts ); // Props tipo Number
 // Rutas principales
 postRouter.post     ( '/',      P.createPost ); // CREATE
-postRouter.get      ( '/:id?',  P.getPosts   ); // READ
-postRouter.put      ( '/:id?',  P.updatePost ); // UPDATE
-postRouter.delete   ('/:id?',   P.deletePost ); // DELETE
+postRouter.get      ( '/:postId?',  P.getPosts   ); // READ
+postRouter.put      ( '/:postId?',  P.updatePost ); // UPDATE
+postRouter.delete   ('/:postId?',   P.deletePost ); // DELETE
 
 export default postRouter;
