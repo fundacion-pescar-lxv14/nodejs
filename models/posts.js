1ec40ba0ee67907dde9b540d1ba29646affcf445
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-24 14:52:12 -0300

    modelo de datos de publicacion

--- models/posts.js
+++ models/posts.js
@@ -1,43 +1,45 @@
-import { posts } from '../data/posts.js';
-
-function Post(post){
-  Object.keys(post).forEach(k => this[k] = Post[k])
-}
-
-Post.Create = (post, callBack) => {
-  posts.push(post)
-  return callBack(null, {message: "publicacion exitosa"})
-}
-Post.Read = (id, key, callBack) => {
-  const data = !id ? posts :
-    posts.filter(p => p.postId == id || p[key].toLowerCase() == id.toLowerCase())
-  if (data.length != 0) return callBack(null, data)
-  else return callBack({message: "no se encontro la publicacion"}, null);
-}
-Post.Filter = ({min, max, stock}, callBack) => {
-  const data = []
-  for(let i in posts) if( 
-      max ? posts[i].price <= max : true &&
-      min ? posts[i].price >= min : true &&
-      stock ? posts[i].stock >= stock : true
-  ) data.push(posts[i])
-  return callBack(null, data)
-}
-Post.Update = (id, data, callBack) => {
-  for(let i in posts)
-  if(posts[i].postId == id){ 
-    posts[i] = data 
-    return callBack(null, {message: "actualizacion exitosa", post: posts[i]})
+import mongoose from "mongoose";
+const postSchema = new mongoose.Schema({
+  postId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  postTitle: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  product: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.0000,
+    max: 99999999.9999,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+  postDate: {
+    type: Date,
+    default: Date.now,
+  },
+  categories: {
+    type: Array, 
+    default: [],
+  },
+  comments: {
+    type: Array,
+    default: []
   }
-  return callBack({message: "No se pudo realizar la actualizacion"}, null)
-}
-Post.Delete = (id, callBack) => {
-  for (let i in posts) 
-  if (posts[i].postId == id){ 
-    posts.splice(i, 1) 
-    return callBack(null, {message: "publicacion eliminada" , total: posts.length})
-  }
-  return callBack({message: "No se pudo eliminar la publicacion"}, null);
-}
-
-export default Post;
+})
+export const Post = mongoose.model('Post', postSchema);
