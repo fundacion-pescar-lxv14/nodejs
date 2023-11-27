25801cab86ced2bdc2dd6589c62f1b276f6621d3
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-27 15:30:34 -0300

    utilizar hbs con parcial de navegacion

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+/** IMPORTACIONES */
+// Production (Permanece en el proyecto)
 import express from 'express'
+import { engine } from 'express-handlebars';
+// Development (Solo para desarrollo)
+import morgan from 'morgan';
+// Database Connection
 import dbConn from './config/db.js';
+// Application Routers
 import userRouter from './routes/users.js'
 import postRouter from './routes/posts.js'
-import morgan from 'morgan';
 
+/** DECLARACIONES */
+// Servidor BackEnd
 const app = express();
 const PORT = process.env.PORT || 3000;
 const startMsg = `Aplicacion ejecutandose en el puerto ${PORT}`
+// Motor de Plantillas
+app.engine("hbs", engine({ defaultLayout: '../index', extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set('views', './views');
 
+/** IMPLEMENTACION */
 app.use(morgan('dev'));
 // Request Body Parser (Middleware)
 app.use(express.urlencoded({extended: true}));
@@ -16,5 +29,6 @@ app.use("/users", userRouter );
 app.use("/posts", postRouter );
 // Rutas Estaticas para el Frontend
 app.use("/", express.static("public"));
+app.use("/dist", express.static("./node_modules/bootstrap/dist"));
 
 app.listen(PORT, console.log(startMsg))
