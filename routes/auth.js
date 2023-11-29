77cc8c830c570e5624c9fbf43a7b71295b5146c3
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-11-29 19:43:18 -0300

    actualizar rutas de registro e inicio de sesion

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import * as Auth from "../controllers/auth.js"
+const authRouter = Router();
+
+authRouter.get  ( '/login',  Auth.LogIn    ); // VISTA ACCESO
+authRouter.post ( '/login',  Auth.Validate ); // VALIDAR ACCESO
+authRouter.get  ( '/signin', Auth.SignIn   ); // VISTA REGISTRO
+authRouter.post ( '/signin', Auth.Register ); // REGISTRAR USUARIO
+
+export default authRouter;
